6b4918fd645970452402464c4f5fc14c01ac88e0
akashzamnani05 <zamnaniakash@gmail.com>
2025-04-15 23:28:30 +0530

    first commit

---
+++ gui/server.py
@@ -0,0 +1,28 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import webbrowser
+import os
+
+def run_server():
+    # Get the current directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Change to the current directory
+    os.chdir(current_dir)
+    
+    # Create server
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    
+    # Open browser
+    webbrowser.open('http://localhost:8000')
+    
+    # Start server
+    print("Server running at http://localhost:8000")
+    print("Press Ctrl+C to stop the server")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer stopped.")
+
+if __name__ == '__main__':
+    run_server() 
